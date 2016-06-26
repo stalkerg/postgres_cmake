2794759cdb67f46b01f60ecb63a17e59adbd6460
Yury Zhuravlev <stalkerg@gmail.com>
2016-06-27 00:42:03 +0300

    Skip C99 arrays test for MSVC

--- cmake/CheckFlexibleArray.cmake
+++ cmake/CheckFlexibleArray.cmake
@@ -9,22 +9,24 @@ set(C99_C_FLAG_CANDIDATES
 )
 
 message(STATUS "Check flexible array support")
-foreach(FLAG ${C99_C_FLAG_CANDIDATES})
-	set(CMAKE_REQUIRED_FLAGS "${FLAG}")
-	unset(FLEXIBLE_ARRAY_MEMBER CACHE)
-	CHECK_C_SOURCE_COMPILES("
-		int main(void){
-			int x = 10;
-			int y[x];
-			return 0;
-		}
-	" FLEXIBLE_ARRAY_MEMBER)
-	if(FLEXIBLE_ARRAY_MEMBER)
-		set(C99_C_FLAGS_INTERNAL "${FLAG}")
-		set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${FLAG}")
-		break()
-	endif(FLEXIBLE_ARRAY_MEMBER)
-endforeach(FLAG ${C99_C_FLAG_CANDIDATES})
+if(NOT MSVC)
+	foreach(FLAG ${C99_C_FLAG_CANDIDATES})
+		set(CMAKE_REQUIRED_FLAGS "${FLAG}")
+		unset(FLEXIBLE_ARRAY_MEMBER CACHE)
+		CHECK_C_SOURCE_COMPILES("
+			int main(void){
+				int x = 10;
+				int y[x];
+				return 0;
+			}
+		" FLEXIBLE_ARRAY_MEMBER)
+		if(FLEXIBLE_ARRAY_MEMBER)
+			set(C99_C_FLAGS_INTERNAL "${FLAG}")
+			set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${FLAG}")
+			break()
+		endif(FLEXIBLE_ARRAY_MEMBER)
+	endforeach(FLAG ${C99_C_FLAG_CANDIDATES})
+endif()
 
 if(FLEXIBLE_ARRAY_MEMBER)
 	message(STATUS "Check flexible array support - yes with ${C99_C_FLAGS_INTERNAL}")
